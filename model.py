3f9ea9c48558d054eaceff74cad5706f58f863eb
realzh <realrealzh@qq.com>
2024-05-23 21:38:27 +0800

    retrain in zh's pc and export params

--- model.py
+++ model.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
             f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
         )
 
-    epochs = 5
+    epochs = 10
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train(train_dataloader, model, loss_fn, optimizer)
